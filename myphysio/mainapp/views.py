6917aaafbfc7b3e2660ac3ca3d9d93f4e4199409
Gowtty <mendozajeancarlo02@gmail.com>
2024-10-07 15:26:18 -0600

    Agregados los archivos para utilizar Django, avances de template de inicio de sesión.

---
+++ myphysio/mainapp/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse, Http404
+from django.template import loader
+
+# Create your views here.
+
+def index(request):
+    template = loader.get_template("./index.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def aboutus(request):
+    template = loader.get_template("./aboutus.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def login(request):
+    template = loader.get_template("./login.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
