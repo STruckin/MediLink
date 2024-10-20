85497ee4e971320a35984ec8fe6517999e624b69
Luis Montiel Urias <luisurias98@gmail.com>
2024-10-19 17:33:45 -0700

    Nueva de pagina de inicio
    Agregue contenido a la pagina de inicio y también una header y footer fijo para los otras páginas.

--- myphysio/mainapp/views.py
+++ myphysio/mainapp/views.py
@@ -3,6 +3,11 @@ from django.template import loader
 
 # Create your views here.
 
+def base(request):
+    template = loader.get_template("./index.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
+
 def index(request):
     template = loader.get_template("./index.html")
     context = {}
