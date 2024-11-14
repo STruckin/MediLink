3812de176c4e61e80a330f0f772badf23dc8bbc3
Luis Montiel Urias <luisurias98@gmail.com>
2024-11-13 22:44:44 -0700

    cambio de base de datos a sqlite

--- myphysio/myphysio/settings.py
+++ myphysio/myphysio/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'myphysio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myphysio',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
